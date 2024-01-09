c6005d84f470af527875cf9f15cf512ac55dc9e8
linjiacheng <1507906763@qq.com>
2024-01-09 13:56:36 +0800

    1.2 格式化

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 const App = createApp({
-  onShow() {
-  },
+  onShow() {},
   // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
 });
 App.use(createPinia());
